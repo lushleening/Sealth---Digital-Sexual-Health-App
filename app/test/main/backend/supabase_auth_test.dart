f20a0485fac304e5abaaff0a3b4971f46434a164
Abdul Hadi Ghani <ahg1g22@soton.ac.uk>
2026-04-01 23:39:16 +0800

    tests added, slight ui updates

--- app/test/main/backend/supabase_auth_test.dart
+++ app/test/main/backend/supabase_auth_test.dart
@@ -1,7 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:sddp_dsh/backend/articles/providers/article.dart';
+import 'package:sddp_dsh/backend/articles/providers/article_filter_provider.dart';
+import 'package:sddp_dsh/backend/articles/providers/article_search_provider.dart';
+import 'package:sddp_dsh/backend/articles/providers/articles_provider.dart';
+import 'package:sddp_dsh/backend/articles/providers/bookmarks_provider.dart';
 import 'package:sddp_dsh/backend/authentication/supabase/supabase_auth.dart';
 import 'package:sddp_dsh/backend/constants/supabase.dart';
 import 'package:sddp_dsh/backend/database/pgsql_supabase/supabase_service.dart';
@@ -135,4 +141,67 @@ void main() {
       () => mockClient.auth.updateUser(UserAttributes(password: mockPassword)),
     ).called(1);
   });
+
+  test("toggleBookmark adds article when not bookmarked", () {
+  final container = ProviderContainer();
+
+  final article = Article(
+    articleId: "1",
+    authorId: "1",
+    title: "Test",
+    content: "Test content",
+    image: "",
+    markdownUrl: "",
+    category: "General",
+    linkToSubpage: const SizedBox(),
+  );
+
+  container.read(bookmarksProvider.notifier).toggleBookmark(article);
+
+  final bookmarks = container.read(bookmarksProvider);
+
+  expect(bookmarks.contains(article), true);
+});
+
+test("toggleBookmark removes article when already bookmarked", () {
+  final container = ProviderContainer();
+
+  final article = Article(
+    articleId: "1",
+    authorId: "1",
+    title: "Test",
+    content: "Test",
+    image: "",
+    markdownUrl: "",
+    category: "General",
+    linkToSubpage: const SizedBox(),
+  );
+
+  container.read(bookmarksProvider.notifier).toggleBookmark(article);
+  container.read(bookmarksProvider.notifier).toggleBookmark(article);
+
+  final bookmarks = container.read(bookmarksProvider);
+
+  expect(bookmarks.isEmpty, true);
+});
+
+test("search provider updates query", () {
+  final container = ProviderContainer();
+
+  container.read(articleSearchProvider.notifier).setSearch("HIV");
+
+  final query = container.read(articleSearchProvider);
+
+  expect(query, "hiv");
+});
+
+test("filter provider updates category", () {
+  final container = ProviderContainer();
+
+  container.read(articleFilterProvider.notifier).setFilter("Testing");
+
+  final filter = container.read(articleFilterProvider);
+
+  expect(filter, "Testing");
+});
 }
